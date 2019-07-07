8f6aae516531f9c0fc3956ffdf76dd2978ab62c8
thephilgray <thephilgray@gmail.com>
2019-07-07 18:01:39 -0400

    use pure html contact form with netlify

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,15 +1,12 @@
 import React from "react"
-// import React, { useState } from "react"
-import { Formik } from "formik"
 import {
   Container,
-  // Typography,
+  Typography,
   Grid,
   Button,
   TextField,
 } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
-// import Loader from "../components/Loader"
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -20,122 +17,74 @@ const useStyles = makeStyles(theme => ({
 
 export default function ContactForm() {
   const classes = useStyles()
-  // const [submitted, setSubmitted] = useState(false)
 
   return (
-    <Formik
-      initialValues={{
-        name: "",
-        email: "",
-        phone: "",
-        message: "",
-      }}
-      validate={values => {
-        let errors = {}
-        if (!values.email) {
-          errors.email = "Required"
-        } else if (
-          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-        ) {
-          errors.email = "Invalid email address"
-        }
-        return errors
-      }}
-      // onSubmit={(values, { setSubmitting }) => {
-      //   // setTimeout(() => {
-      //   //   console.log(JSON.stringify(values, null, 2))
-      //   // }, 4000)
-      //     setSubmitting(false)
-      //     setSubmitted(true)
-      // }}
-    >
-      {({
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting,
-        isValid,
-
-        /* and other goodies */
-      }) => {
-        return (
-          <Container className={classes.root}>
-            {/* <Typography variant="h4" align="center" gutterBottom>
-              {submitted && `Got it! Talk to you soon.`}
-              {isSubmitting && `Sending...`}
-              {!submitted && !isSubmitting && `Send us a message`}
-            </Typography>
-            {isSubmitting && <Loader />} */}
-            {/* {!isSubmitting && !submitted && ( */}
-              <form name="contact" action="/contact/success/" method="post" data-netlify="true">
-                <Grid container spacing={2}>
-                  <Grid item xs={12} md={12}>
-                    <TextField
-                      label="Name"
-                      type="text"
-                      name="name"
-                      variant="outlined"
-                      required
-                      onChange={handleChange}
-                      value={values.name}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12} md={6}>
-                    <TextField
-                      type="email"
-                      name="email"
-                      label="Email Address"
-                      variant="outlined"
-                      required
-                      onChange={handleChange}
-                      value={values.email}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12} md={6}>
-                    <TextField
-                      label="Phone"
-                      type="tel"
-                      name="phone"
-                      variant="outlined"
-                      onChange={handleChange}
-                      value={values.phone}
-                      fullWidth
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      rows="4"
-                      multiline
-                      label="Message"
-                      name="message"
-                      variant="outlined"
-                      onChange={handleChange}
-                      value={values.message}
-                      fullWidth
-                    />
-                  </Grid>
-                </Grid>
-                <Button
-                  fullWidth
-                  style={{ marginTop: "1em" }}
-                  color="primary"
-                  variant="contained"
-                  type="submit"
-                  size="large"
-                  disabled={!isValid}
-                >
-                  Contact
-                </Button>
-              </form>
-            {/* )} */}
-          </Container>
-        )
-      }}
-    </Formik>
+    <Container className={classes.root}>
+      <Typography variant="h4" align="center" gutterBottom>
+        Send us a message
+      </Typography>
+      <form
+        name="contact"
+        action="/contact/success"
+        method="post"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="bot-field" />
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={12}>
+            <TextField
+              label="Name"
+              type="text"
+              name="name"
+              variant="outlined"
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              type="email"
+              name="email"
+              label="Email Address"
+              variant="outlined"
+              required
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <TextField
+              label="Phone"
+              type="tel"
+              name="phone"
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              rows="4"
+              multiline
+              label="Message"
+              name="message"
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+        </Grid>
+        <Button
+          fullWidth
+          style={{ marginTop: "1em" }}
+          color="primary"
+          variant="contained"
+          type="submit"
+          name="submit"
+          value="Send"
+          size="large"
+        >
+          Contact
+        </Button>
+      </form>
+    </Container>
   )
 }

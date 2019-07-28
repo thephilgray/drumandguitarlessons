724f27660d0180255a30bcc82380fc3ea94705eb
thephilgray <thephilgray@gmail.com>
2019-07-28 18:40:36 -0400

    enable service worker / pwa support

--- gatsby-config.js
+++ gatsby-config.js
@@ -91,7 +91,7 @@ module.exports = (async () => {
         },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
+        `gatsby-plugin-offline`,
       ],
     }
   })
